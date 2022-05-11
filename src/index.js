d5748c3341f10ae102a0e94c251c86b26c164cb3
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2022-05-11 23:40:47 +0530

    feat: add last-resort err handling

--- src/index.js
+++ src/index.js
@@ -2,17 +2,33 @@ const app = require('./app.js')
 const config = require('./config.js')
 const logger = require('./lib/logger.js')
 
-const startServer = ({ serverStartLogger, config, app }) => {
+const startServer = ({ serverStartLogger, anomalyLogger, config, app }) => {
   app.listen(config.getPort(), () => {
     serverStartLogger.info({
       msg: `Server listening on ${config.getPort()}`,
     })
   })
+
+  process.on('uncaughtException', (error) => {
+    anomalyLogger.error(error)
+    process.exit(1)
+  })
+
+  process.on('unhandledRejection', (reason, promise) => {
+    anomalyLogger.error({
+      reason,
+      promise,
+    })
+    process.exit(1)
+  })
 }
 
 startServer({
   serverStartLogger: logger({
-    tag: 'server_logger',
+    tag: 'server_logs',
+  }),
+  anomalyLogger: logger({
+    tag: 'uncaught_error_logs',
   }),
   config,
   app,
